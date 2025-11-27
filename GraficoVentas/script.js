236ddcca9f30e423055f47cc34ffd65ed76b9957
joaquin-perez-beep <priscilarupcic@gmail.com>
2025-11-27 18:06:30 -0300

    Ultimos cambios
    Graficos

--- GraficoVentas/script.js
+++ GraficoVentas/script.js
@@ -3,17 +3,53 @@ const form = document.getElementById('formVentas');
 
 let ventas = JSON.parse(localStorage.getItem('ventas')) || [];
 
+const data = {
+  labels: [
+    'Red',
+    'Green',
+    'Yellow',
+    'Grey',
+    'Blue'
+  ],
+  datasets: [{
+    label: 'My First Dataset',
+    data: [11, 16, 7, 3, 14],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(75, 192, 192)',
+      'rgb(255, 205, 86)',
+      'rgb(201, 203, 207)',
+      'rgb(54, 162, 235)'
+    ]
+  }]
+};
+
 const chart = new Chart(ctx, {
-  type: 'bar',
+  type: 'polarArea',
   data: {
     labels: ventas.map(v => v.mes),
     datasets: [{
       label: 'Monto de Ventas ($)',
       data: ventas.map(v => v.monto),
-      backgroundColor: '#2e86de'
+      backgroundColor: [
+        'rgb(255, 99, 132)',
+        'rgb(75, 192, 192)',
+        'rgb(255, 205, 86)',
+        'rgb(201, 203, 207)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(255, 159, 64)'
+      ]
     }]
   },
-  options: { responsive: true, scales: { y: { beginAtZero: true } } }
+  options: { 
+    responsive: true,
+    scales: {
+      r: {
+        beginAtZero: true
+      }
+    }
+  }
 });
 
 form.addEventListener('submit', e => {
